949f36a8e1400f124e0331efad5ee3c20dc5be75
AntonTimoshok <a.timoshok@yandex.ru>
2022-11-23 18:55:01 +0700

    добавил возможность выбора разделителя при записи в файл

--- File_handling.py
+++ File_handling.py
@@ -2,10 +2,11 @@ from Contact_Processing import Add_contact
 
 
 def write_con(contacts, file_name):
+  separator = input('Каким символом разделить строки? ')
   file = open(file_name, 'a', encoding='utf-8')
   for contact in contacts:
     for elem in contact:
-      file.write(str(elem) + ';')
+      file.write(str(elem) + separator)
     file.write('\n')
   file.close()
   print(f'База успешно записана в "{file_name}".')
