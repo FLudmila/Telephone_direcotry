5b7f688631b7520baaf99c71a21a8c5025cc5204
AntonTimoshok <a.timoshok@yandex.ru>
2022-12-02 21:39:07 +0700

    Начинаем добавлять ТГ

--- Main.py
+++ Main.py
@@ -4,12 +4,19 @@ from User_Interface import User_Comand
 
 
 def main():
-    base = []
-    while True:
-        comand = User_Comand(base)
-        input_comand = comand[0]
-        if input_comand == 0:
-            break
+    try:
+        mode = int(input('1 - Консольный справочник\n2 - Справочник в Телеграм\nВыбрите режим: '))
+        if mode == 1:
+            base = []
+            while True:
+                comand = User_Comand(base)
+                input_comand = comand[0]
+                if input_comand == 0:
+                    break
+        elif mode == 2:
+            print('Run Telegram')
+    except:
+        print('[!] Такого режима нет.')
 
 if __name__ == '__main__':
     main()
